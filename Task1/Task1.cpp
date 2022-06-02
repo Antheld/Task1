6100fa60aede7f38c8641f265193e51c33799be8
Antheld <89795541+Antheld@users.noreply.github.com>
2022-06-02 16:07:13 +0300

    Добавьте файлы проекта.

---
+++ Task1/Task1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int isPrimeNumber(int x);
+
+int SumOfPrimeNumber(int x) {
+    int sum = 0;
+
+    while (x > 1) {
+        if (isPrimeNumber(x)) sum += x;
+        x--;
+    };
+    return sum;
+}
+
+int isPrimeNumber(int x) {
+    for (int i = 2; i < x; ++i) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int x;
+    std::cin >> x;
+    if (x > 1) return SumOfPrimeNumber(x);
+    else return 0;
+}
